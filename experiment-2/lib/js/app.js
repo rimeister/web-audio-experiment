c78a051f8f27eb16d925b4e541fdb7548cf44947
Riley Boyd <riley.boyd@catoand-ajeng.ad.ddb.ca>
2018-01-11 15:28:11 -0500

    Adding files for experiment #2: End goal is to have objects (e.g., circles) appear on the screen when a note is played. Using P5.js, need to make class for circle/shape objects

---
+++ experiment-2/lib/js/app.js
@@ -0,0 +1,143 @@
+// Web Audio API experiment
+
+/****************************
+******** Variables **********
+/***************************/
+
+var startBtn = document.getElementById("start");
+var stopBtn = document.getElementById("stop");
+var noteBtns = document.getElementsByClassName("playNote");
+
+// Frequencies of notes in the C Major scale, from C4 to C5
+var cMajorScale = [261.6,293.7,329.6,349.2,392.0,440.0,493.9,523.3]
+
+// const for milliseconds in a minute
+var millisecondsPerMinute = 60000;
+
+
+/****************************
+******** Classes ************
+/***************************/
+
+// Voltage Controlled Oscillator (VCO) definition
+function VCO(context) {
+	this.oscillator = context.createOscillator();
+	this.oscillator.type = 'triangle';
+	this.setFrequency(440);
+	this.input = this.oscillator;
+	this.output = this.oscillator;
+	this._hasStarted = false;
+};
+
+VCO.prototype.start = function(){
+	if (!this._hasStarted) {
+		this.oscillator.start(0);	
+		this._hasStarted = true;	
+	}
+}
+
+VCO.prototype.stop = function(){
+	if (this._hasStarted) {
+		this.oscillator.stop(0);
+	}
+}
+
+VCO.prototype.setFrequency = function(frequency) {
+	this.oscillator.frequency.setValueAtTime(frequency, context.currentTime);
+};
+
+VCO.prototype.connect = function(node) {
+	if (node.hasOwnProperty('input')) {
+		this.output.connect(node.input);
+	} else {
+		this.output.connect(node);
+	};
+}
+
+// Melody generator definition
+function Generator(vco,bpm,subdivision) {
+	// Pass in the vco you want the generator to act upon
+	this.vco = vco;
+	this.bpm = bpm;
+	this.subdivision = subdivision;
+	this._timeout;
+	this._generatorRunning = false;
+}
+
+Generator.prototype = {
+	
+	constructor: Generator,
+	start: function() {
+
+		var thisGenerator = this;
+
+		function changeNote() {
+			var randomInt = getRandomNoteDuration(60,16);
+		    thisGenerator._timeout = setTimeout(changeNote, randomInt);
+		    thisGenerator.vco.setFrequency( cMajorScale[Math.floor((Math.random() * 8))] ); // put random note here
+		}
+		
+		this._timeout = setTimeout(changeNote, getRandomNoteDuration(60,16));
+
+	},
+	stop: function() {
+		var thisGenerator = this;
+		this._generatorRunning = false;
+		clearTimeout(this._timeout);
+	}
+
+}
+
+
+/****************************
+******* Main Program ********
+/***************************/
+
+// Add event listeners to start and stop buttons
+startBtn.addEventListener("click",function(event){
+	event.preventDefault();
+	myVCO.start(0);
+	myGenerator.start();
+});
+
+stopBtn.addEventListener("click",function(event){
+	event.preventDefault();
+	myGenerator.stop();
+	myVCO.stop(0);
+});
+
+// Create audio context for the application
+var context = new AudioContext; 
+
+// Make an oscilattor using the VCO class
+var myVCO = new VCO(context);
+
+// Connect the oscillator to the context
+myVCO.connect(context.destination);
+
+// Make new melody generator, 60bpm, 8 = eigth notes
+var myGenerator = new Generator(myVCO,60,8);
+
+function setup() {
+	createCanvas(window.innerWidth,window.innerHeight)
+}
+
+/****************************
+******** Functions **********
+/***************************/
+
+// Function that returns a random note duration. Returns a note that 
+// is between the subdivision length (e.g., 16, 8, 4, 2) and a whole note.
+function getRandomNoteDuration(bpm,subdivision) {
+
+	// length of one beat in ms
+	var lengthOfOneBeat = millisecondsPerMinute/bpm;
+
+	// Get a random power of 2 -- 1,2,4,8,16
+	var randomDivision = Math.pow( 2, Math.floor( Math.random() * ( Math.log2(subdivision) + 1) ) );
+
+	// Divide the length of one beat by a random division
+	var randomDuration = lengthOfOneBeat / randomDivision;
+	
+	return randomDuration;
+}
